c18f82289145e1503932efff6c70a71aff77a118
Grisha <guruwolf8@gmail.com>
2023-06-09 23:09:14 +0300

    Задача 1.

--- src/Methods.java
+++ src/Methods.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Methods {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void checkYears() {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        boolean checkYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        if (checkYear) {
+            System.out.println(year + " год - високосный год");
+        }else {
+            System.out.println(year+ " год- невисокосный год");
+        }
     }
-}
+
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1.");
+        checkYears();
+        }
+    }
