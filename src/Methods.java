fafe3f75d4ef9bb33e28b3b25bf5afe504729c49
Grisha <guruwolf8@gmail.com>
2023-06-09 23:27:41 +0300

    Задача 3.

--- src/Methods.java
+++ src/Methods.java
@@ -8,16 +8,17 @@ public class Methods {
         boolean checkYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
         if (checkYear) {
             System.out.println(year + " год - високосный год");
-        }else {
-            System.out.println(year+ " год- невисокосный год");
+        } else {
+            System.out.println(year + " год- невисокосный год");
         }
     }
-    public static void checkDevice(){
+
+    public static void checkDevice() {
         int clientOs = 1;
         int android = 1;
         int iOS = 0;
-        int currentYear= LocalDate.now().getYear();
-        Scanner scanner=new Scanner(System.in);
+        int currentYear = LocalDate.now().getYear();
+        Scanner scanner = new Scanner(System.in);
         int clientDeviceYear = scanner.nextInt();
         if (clientOs == iOS && clientDeviceYear >= currentYear) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
@@ -31,17 +32,38 @@ public class Methods {
             //Проверенно всеми 4 вариантами
         }
     }
+    public static void checkDelivery(){
+        Scanner scanner=new Scanner(System.in);
+        int deliveryDistance = scanner.nextInt();
+        int deliveryDays = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + deliveryDays + ".");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: " + (deliveryDays + 1) + ".");
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: " + (deliveryDays + 2) + ".");
+        }else{
+            System.out.println("Доставки нет.");
+        }
+    }
 
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
         System.out.println("Задача 1.");
-   //     checkYears();
-        }
-        public static void task2(){
-        checkDevice();
-        }
+        //     checkYears();
     }
+
+    public static void task2() {
+        System.out.println("Задача 2.");
+        //checkDevice();
+    }
+    public static void task3(){
+        System.out.println("Задача 3.");
+checkDelivery();
+    }
+}
