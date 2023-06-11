91a5e1c618bb381bdf72ad6cdd52c6b5ed7e9665
Grisha <guruwolf8@gmail.com>
2023-06-11 13:44:41 +0300

    Исправил. Надеюсь правильно понял пожелания к поправкам

--- src/Methods.java
+++ src/Methods.java
@@ -1,52 +1,8 @@
+import javax.swing.*;
 import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Methods {
-    public static void checkYears() {
-        Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
-        boolean checkYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-        if (checkYear) {
-            System.out.println(year + " год - високосный год");
-        } else {
-            System.out.println(year + " год- невисокосный год");
-        }
-    }
-
-    public static void checkDevice() {
-        int clientOs = 1;
-        int android = 1;
-        int iOS = 0;
-        int currentYear = LocalDate.now().getYear();
-        Scanner scanner = new Scanner(System.in);
-        int clientDeviceYear = scanner.nextInt();
-        if (clientOs == iOS && clientDeviceYear >= currentYear) {
-            System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientOs == iOS && clientDeviceYear < currentYear) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-        }
-        if (clientOs == android && clientDeviceYear >= currentYear) {
-            System.out.println("Установите версию приложения для Android по ссылке.");
-        } else if (clientOs == android && clientDeviceYear < currentYear) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            //Проверенно всеми 4 вариантами
-        }
-    }
-    public static void checkDelivery(){
-        Scanner scanner=new Scanner(System.in);
-        int deliveryDistance = scanner.nextInt();
-        int deliveryDays = 1;
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " + deliveryDays + ".");
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: " + (deliveryDays + 1) + ".");
-        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: " + (deliveryDays + 2) + ".");
-        }else{
-            System.out.println("Доставки нет.");
-        }
-    }
-
     public static void main(String[] args) {
         task1();
         task2();
@@ -54,16 +10,85 @@ public class Methods {
     }
 
     public static void task1() {
-        System.out.println("Задача 1.");
-        //     checkYears();
+        System.out.println("Задача 1");
+        Scanner scanner=new Scanner(System.in);
+        int year=scanner.nextInt();
+        printCheckYear(year);
+
+    }
+
+    public static void printCheckYear(int year) {
+        boolean checkYear=year%4==0&&year%100!=0||year%400==0;
+        if (checkYear){
+            System.out.println(year+" год - високосный год");
+        }else{
+            System.out.println(year+" год - невисокосный год");
+        }
+
     }
 
     public static void task2() {
-        System.out.println("Задача 2.");
-        //checkDevice();
+        System.out.println("Задача 2");
+        Scanner scanner = new Scanner(System.in);
+        int clientOs = scanner.nextInt();
+        int clientDeviceYear = scanner.nextInt();
+        printVersionType(clientOs, clientDeviceYear);
+
+    }
+
+    public static void printVersionType(int clientOs, int clientDeviceYear) {
+        int iOs = 0;
+        int android = 1;
+        int currentYear = LocalDate.now().getYear();
+
+        if (clientOs > android && clientOs > iOs) {
+            throw new RuntimeException("Такая OS не поддерживается!!!");
+        }
+        if (clientDeviceYear < 2010) {
+            throw new RuntimeException("Купи новый телефон,твой слишком старый");
+        }
+
+        if (clientOs == iOs) {
+            System.out.print("iOs - ");
+
+            if (clientDeviceYear < currentYear) {
+                System.out.println("облегченная");
+            } else {
+                System.out.println("обычная");
+            }
+        }
+        if (clientOs == android) {
+            System.out.print("android - ");
+            if (clientDeviceYear < currentYear) {
+                System.out.println("облегченная");
+            } else {
+                System.out.println("обычная");
+            }
+        }
     }
     public static void task3(){
-        System.out.println("Задача 3.");
-checkDelivery();
+        System.out.println("Задача 3");
+        Scanner scanner=new Scanner(System.in);
+        int deliveryDistance=scanner.nextInt();
+        int days=checkDeliveryTime(deliveryDistance);
+        if (days>0) {
+            System.out.println(days);
+        }else {
+            System.out.println(days);
+        }
+
+    }
+    public static int checkDeliveryTime(int deliveryDistance){
+        int deliveryTime=1;
+        if (deliveryDistance<20){
+            deliveryTime=1;
+        }else if (deliveryDistance<60) {
+            deliveryTime=2;
+        }else if (deliveryDistance<100) {
+            deliveryTime=3;
+        }else{
+            deliveryTime=-1;
+        }
+return deliveryTime;
     }
 }
